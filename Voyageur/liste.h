30312533a943033acdff949296317642a6fbfa4a
mordroreur <mlaurencot@gmail.com>
2023-06-27 18:09:37 +0200

    implemente liste chainées

--- Voyageur/liste.h
+++ Voyageur/liste.h
@@ -13,13 +13,20 @@ typedef struct maillon
 
 typedef struct liste{
   maillon *deb;
-  maillon *fin;
   int taille;
 }liste;
 
 
-liste *LL_create();
+liste LL_create();
 void LL_add_first(liste *l, int val);
-void LL_free(liste * l);
+void LL_free(liste *l);
+int LL_contains(liste *l, int n);
+int LL_size(liste *l);
+void LL_afficheListe(liste * l);
+void LL_add_last(liste *l, int val);
+int LL_get_n(liste *l, int index);
+void LL_remove_n(liste *l, int index);
+void LL_remove_head(liste *l);
+void LL_remove_tail(liste *l);
 
 #endif /*__GUARDIAN_LISTE_HEADER__*/

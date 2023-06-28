4bce78c4bafd0cbba5567184dc4b31cf91cc1a60
Falquage <tristan.pierre.claudel@gmail.com>
2023-06-28 08:32:08 +0200

    Mise à jour mingraph

--- Voyageur/ticksGest.c
+++ Voyageur/ticksGest.c
@@ -21,11 +21,11 @@ void *ChercheMinGraphe(void *param){
     return NULL;
 }
 
-char ** createTab() {
-  char ** Tab = (char **) malloc(sizeof(char *)*N); // alloue le tableau 1D
+float ** createTab() {
+  float ** Tab = (float **) malloc(sizeof(float *)*N); // alloue le tableau 1D
   if(Tab == NULL) errorInCreate2D();                // appelle fonction qui envoie message + quit prog si NULL (malloc impossible)
   for(int i = 0; i < N; i++) {
-    Tab[i] = (char *) malloc(sizeof(char)*N);       // alloue le tab2D
+    Tab[i] = (float *) malloc(sizeof(float)*N);       // alloue le tab2D
     if(Tab[i] == NULL) {                            // free proprement + appelle fonction qui envoie message + quit prog si NULL (malloc impossible)
       for(int j = 0; j < i; j++) {
         free(Tab[j]); 
@@ -45,18 +45,41 @@ char ** createTab() {
 
 
 float **TransfGraphCompl(float ** G){
-  float ** GC = createTab();
+  float ** GCompl = createTab();
   for k in range(N){
     for i in range(N){
       for j in range(N){
         if(k==0){
           if(G[i][j]==0){ GC[i][j]=INFINI;}
-          else {GC[i][j]=G[i][j]}
+          else {GCompl[i][j]=G[i][j]}
         }
         else{
-          GC[i][j]=min(GC[i][j],GC[i][k-1] + GC[k-1][j]);
+          GCompl[i][j]=min(GC[i][j],GC[i][k-1] + GC[k-1][j]);
         }
       }
     }
   }
+  return(GCompl);
+}
+
+float TestSolution(liste * Soluce,float ** GC){
+  float poids = 0;
+  int i = 0;
+  int j = 0;
+  for l in range(N-1){
+    i = Soluce[l];
+    j = Soluce[l+1];
+    poids += GC[i][j]
+  }
+  poids += GC[N][0];
+
+  return(poids);
+}
+
+liste * Randlist(int N,int nbliste){
+
+}
+
+liste AlgoGene(float ** GC){
+
 }
